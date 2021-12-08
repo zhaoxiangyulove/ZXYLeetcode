54ea0cc83b7f708300350e55ffa9c7d9159efd53
zhaoxiangyu <44899845@qq.com>
2021-12-08 15:53:16 +0800

    fix 689

--- MyLibrary/Sources/MyLibrary/Leetcode.swift
+++ MyLibrary/Sources/MyLibrary/Leetcode.swift
@@ -165,7 +165,7 @@ class Solution {
       $0 < "a"
     } == nil
   }
-
+  
   func getMoneyAmount(_ n: Int) -> Int {
     var dp = [[Int]](repeating: [Int](repeating: 0, count: n + 1), count: n + 1)
     for i in stride(from: n - 1, to: 0, by: -1) {
@@ -504,6 +504,41 @@ class Solution {
       return false
     }
     return grid[nearRow][nearCol] == initColor
+  }
+  
+  func maxSumOfThreeSubarrays(_ nums: [Int], _ k: Int) -> [Int] {
+    var ans = [Int](repeating: 0, count: 3)
+    
+    var sum1 = 0, maxSum1 = 0, maxSum1Idx = 0
+    var sum2 = 0, maxSum2 = 0, maxSum2Idx1 = 0, maxSum2Idx2 = 0
+    var sum3 = 0, maxTotal = 0
+    
+    for i in (k * 2) ..< nums.count {
+      sum1 += nums[i - k * 2]
+      sum2 += nums[i - k]
+      sum3 += nums[i]
+      if i >= k * 3 - 1 {
+        if sum1 > maxSum1 {
+          maxSum1 = sum1
+          maxSum1Idx = i - k * 3 + 1
+        }
+        if maxSum1 + sum2 > maxSum2 {
+          maxSum2 = maxSum1 + sum2
+          maxSum2Idx1 = maxSum1Idx
+          maxSum2Idx2 = i - k * 2 + 1
+        }
+        if maxSum2 + sum3 > maxTotal {
+          maxTotal = maxSum2 + sum3
+          ans[0] = maxSum2Idx1
+          ans[1] = maxSum2Idx2
+          ans[2] = i - k + 1
+        }
+        sum1 -= nums[i - k * 3 + 1]
+        sum2 -= nums[i - k * 2 + 1]
+        sum3 -= nums[i - k + 1]
+      }
+    }
+    return ans
   }
 }
 
--- MyLibrary/Tests/MyLibraryTests/LeetcodeTests.swift
+++ MyLibrary/Tests/MyLibraryTests/LeetcodeTests.swift
@@ -169,4 +169,9 @@ final class LeetcodeTestCase: XCTestCase {
     XCTAssertEqual(solution.colorBorder([[1,2,2],[2,3,2]], 0, 1, 3), [[1,3,3],[2,3,3]])
     XCTAssertEqual(solution.colorBorder([[1,1,1],[1,1,1],[1,1,1]], 1, 1, 2), [[2,2,2],[2,1,2],[2,2,2]])
   }
+  
+  func test_689() {
+    XCTAssertEqual(solution.maxSumOfThreeSubarrays([1,2,1,2,6,7,5,1], 2), [0,3,5])
+    XCTAssertEqual(solution.maxSumOfThreeSubarrays([1,2,1,2,1,2,1,2,1], 2), [0,2,4])
+  }
 }
